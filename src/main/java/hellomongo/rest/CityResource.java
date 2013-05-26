acdd6077df586b21ea480f05a536c5949e098614
Xavier Hanin <xavier.hanin@gmail.com>
2013-05-26 10:02:09 +0200

    add CRUD operations on cities

--- src/main/java/hellomongo/rest/CityResource.java
+++ src/main/java/hellomongo/rest/CityResource.java
@@ -1,12 +1,16 @@
 package hellomongo.rest;
 
+import com.google.common.base.Optional;
 import hellomongo.domain.City;
-import restx.annotations.GET;
-import restx.annotations.RestxResource;
+import org.bson.types.ObjectId;
+import restx.Status;
+import restx.annotations.*;
 import restx.factory.Component;
 import restx.jongo.JongoCollection;
 
 import javax.inject.Named;
+
+import static restx.common.MorePreconditions.checkEquals;
 
 @Component @RestxResource
 public class CityResource {
@@ -20,4 +24,29 @@ public class CityResource {
     public Iterable<City> findCities() {
         return cities.get().find().as(City.class);
     }
+
+    @POST("/cities")
+    public City createCity(City city) {
+        cities.get().save(city);
+        return city;
+    }
+
+    @GET("/cities/{oid}")
+    public Optional<City> findCityById(String oid) {
+        return Optional.fromNullable(cities.get().findOne(new ObjectId(oid)).as(City.class));
+    }
+
+    @PUT("/cities/{oid}")
+    public City updateCity(String oid, City city) {
+        checkEquals("oid", oid, "city.key", city.getKey());
+        cities.get().save(city);
+        return city;
+    }
+
+    @DELETE("/cities/{oid}")
+    public Status deleteCity(String oid) {
+        cities.get().remove(new ObjectId(oid));
+        return Status.of("deleted");
+    }
+
 }
--- src/test/java/hellomongo/rest/CitySpecTest.java
+++ src/test/java/hellomongo/rest/CitySpecTest.java
@@ -16,4 +16,24 @@ public class CitySpecTest {
     public void should_find_all_cities() throws Exception {
         rule.runTest("specs/city/should_find_all_cities.spec.yaml");
     }
+    @Test
+    public void should_create_city() throws Exception {
+        rule.runTest("specs/city/should_create_city.spec.yaml");
+    }
+    @Test
+    public void should_update_city() throws Exception {
+        rule.runTest("specs/city/should_update_city.spec.yaml");
+    }
+    @Test
+    public void should_not_update_city_with_no_id() throws Exception {
+        rule.runTest("specs/city/should_not_update_city_with_no_id.spec.yaml");
+    }
+    @Test
+    public void should_find_city_by_id() throws Exception {
+        rule.runTest("specs/city/should_find_city_by_id.spec.yaml");
+    }
+    @Test
+    public void should_delete_city() throws Exception {
+        rule.runTest("specs/city/should_delete_city.spec.yaml");
+    }
 }
