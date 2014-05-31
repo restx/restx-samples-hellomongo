f845e6ec8390436d29bc55ea9081235babcc14e8
Xavier Hanin <xavier.hanin@4sh.fr>
2014-05-31 10:27:02 +0200

    upgrade to restx 0.33.1

--- src/main/java/hellomongo/AppModule.java
+++ src/main/java/hellomongo/AppModule.java
@@ -1,10 +1,10 @@
 package hellomongo;
 
 import com.google.common.base.Charsets;
+import restx.mongo.MongoModule;
 import restx.security.SignatureKey;
 import restx.factory.Module;
 import restx.factory.Provides;
-import restx.jongo.JongoFactory;
 
 import javax.inject.Named;
 
@@ -15,7 +15,7 @@ public class AppModule {
          return new SignatureKey("3685755889849339038 636b37d4-f945-4a6b-8fea-31900e12861a restx-samples-hellomongo restx-samples-hellomongo".getBytes(Charsets.UTF_8));
     }
 
-    @Provides @Named(JongoFactory.JONGO_DB_NAME)
+    @Provides @Named(MongoModule.MONGO_DB_NAME)
     public String dbName() {
         return "restx-hellomongo";
     }
